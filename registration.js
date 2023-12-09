2e622ba52defe496f1b032b40f81e4009b8e150a
ahmedraza <ahmed.raza1054@gmail.com>
2023-12-10 01:24:01 +0500

    Signup login page

--- registration.js
+++
@@ -1,6 +0,0 @@
-const register = () => {
-  event.preventDefault();
-  console.log("testing");
-};
-let registration = document.getElementById("register");
-registration.addEventListener("click", register);
