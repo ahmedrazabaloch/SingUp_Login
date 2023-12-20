b905bbd4cb4ba54a567d0d4706adad6f8e12898a
ahmedraza <ahmed.raza1054@gmail.com>
2023-12-21 03:42:49 +0500

    for clean code

--- app.js
+++ app.js
@@ -5,31 +5,36 @@ import {
   sendEmailVerification,
 } from "/firebase.js";
 
-let userName = document.getElementById("user_name");
+let userDisplayName = document.getElementById("user_name");
 let userEmail = document.getElementById("user_email");
-let logout = document.getElementById("logout_btn");
+let logoutButton = document.getElementById("logout_btn");
 
+const notLoginPages = [
+  "/Login&Signup/login.html",
+  "/Login&Signup/signup.html",
+  "/Login&Signup/loginWithNum.html",
+];
+const loginPages = ["/profile.html", "/index.html"];
 //User Confirmation if they login
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // sendEmailVerification(auth.currentUser).then(() => {
     //   console.log("Email verification sent!");
     // });
-    location.pathname !== "/profile.html"
-      ? (location = "/profile.html")
-      : console.log("Welcome");
 
-    userEmail ? (userEmail.innerHTML = user.email) : userEmail;
-
-    userName
-      ? (userName.innerHTML = user.email.slice(0, user.email.indexOf("@")))
-      : userName;
+    if (!loginPages.includes(location.pathname)) {
+      location = "/profile.html";
+    }
+    if (userEmail) {
+      userEmail.innerHTML = user.email;
+    }
+    if (userDisplayName) {
+      userDisplayName.innerHTML = user.email.slice(0, user.email.indexOf("@"));
+    }
   } else {
-    location.pathname !== "/Login&Signup/login.html" &&
-    location.pathname !== "/Login&Signup/signup.html" &&
-    location.pathname !== "/Login&Signup/loginWithNum.html"
-      ? (location = "/Login&Signup/login.html")
-      : console.log("Can't access without login");
+    if (!notLoginPages.includes(location.pathname)) {
+      location = "/Login&Signup/login.html";
+    }
   }
 });
 
@@ -45,28 +50,22 @@ const userLogout = () => {
     });
 };
 
-logout && logout.addEventListener("click", userLogout);
+logoutButton && logoutButton.addEventListener("click", userLogout);
 
-const showProfile = () => {
-  let showHide = document.getElementById("showHide");
-  if (showHide.style.display === "none") {
-    showHide.style.display = "block";
-  } else {
-    showHide.style.display = "none";
-  }
+const toggleDisplay = (element) => {
+  element.style.display = element.style.display === "none" ? "block" : "none";
 };
 
 const dropdownProfile = document.getElementById("dropdown");
-dropdownProfile && dropdownProfile.addEventListener("click", showProfile);
-
-const navProfile = () => {
-  let mobileNav = document.getElementById("mobile-menu");
-  if (mobileNav.style.display === "none") {
-    mobileNav.style.display = "block";
-  } else {
-    mobileNav.style.display = "none";
-  }
-};
+dropdownProfile &&
+  dropdownProfile.addEventListener("click", () => {
+    const showHide = document.getElementById("showHide");
+    toggleDisplay(showHide);
+  });
 
 const navShowHide = document.getElementById("navShowHide");
-navShowHide && navShowHide.addEventListener("click", navProfile);
+navShowHide &&
+  navShowHide.addEventListener("click", () => {
+    const mobileNav = document.getElementById("mobile-menu");
+    toggleDisplay(mobileNav);
+  });
